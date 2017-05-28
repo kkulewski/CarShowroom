a1e015d1db8c93abc9c2aa66a8a22b42b4d0b98d
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-05-28 23:35:33 +0200

    Add Client model

---
+++ CarShowroom/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace CarShowroom.Models
+{
+	public class Client
+	{
+		public int ClientId { get; set; }
+		public string LastName { get; set; }
+		public string FirstName { get; set; }
+		public string PESEL { get; set; }
+		public string City { get; set; }
+		public string Street { get; set; }
+		public int StreetNumber { get; set; }
+
+		public virtual ICollection<Purchase> Purchases { get; set; }
+	}
+}
