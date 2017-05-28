1c2e7e0fab448c759ba9a20c1a64374e46178319
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-05-28 23:36:07 +0200

    Add Car model

---
+++ CarShowroom/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CarShowroom.Models
+{
+	public class Car
+	{
+		public int CarId { get; set; }
+		public string Brand { get; set; }
+		public string Model { get; set; }
+		public decimal Price { get; set; }
+		public bool IsNew { get; set; }
+
+		public virtual ICollection<Purchase> Purchases { get; set; }
+	}
+}
