aecb93696fef3259bebf5672925ce924a791f6d5
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-05-28 23:36:15 +0200

    Add Purchase model

---
+++ CarShowroom/Models/Purchase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarShowroom.Models
+{
+	public class Purchase
+	{
+		public int PurchaseId { get; set; }
+		public int ClientId { get; set; }
+		public int WorkerId { get; set; }
+		public int CarId { get; set; }
+		public DateTime TransactionDate { get; set; }
+
+		public virtual Client Client { get; set; }
+		public virtual Worker Worker { get; set; }
+		public virtual Car Car { get; set; }
+	}
+}
