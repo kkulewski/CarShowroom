2688d606edbd7b5130649bf300bd98c088ab62b3
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-05-28 23:35:45 +0200

    Add Worker model

---
+++ CarShowroom/Models/Worker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace CarShowroom.Models
+{
+	public class Worker
+	{
+		public int WorkerId { get; set; }
+		public string LastName { get; set; }
+		public string FirstName { get; set; }
+		public string Pesel { get; set; }
+		public string City { get; set; }
+		public string Street { get; set; }
+		public int StreetNumber { get; set; }
+		public int PositionId { get; set; }
+
+		public virtual Position Position { get; set; }
+
+		public virtual ICollection<Purchase> Purchases { get; set; }
+	}
+}
