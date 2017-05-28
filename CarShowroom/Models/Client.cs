d8933f69eb3e6df08111b37c7edceb1d6d2b3ffc
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-05-29 00:10:36 +0200

    Fix Client model uppercase

--- CarShowroom/Models/Client.cs
+++ CarShowroom/Models/Client.cs
@@ -7,7 +7,7 @@ namespace CarShowroom.Models
 		public int ClientId { get; set; }
 		public string LastName { get; set; }
 		public string FirstName { get; set; }
-		public string PESEL { get; set; }
+		public string Pesel { get; set; }
 		public string City { get; set; }
 		public string Street { get; set; }
 		public int StreetNumber { get; set; }
