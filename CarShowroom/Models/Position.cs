040ed04bfdfed49166808bdd27b68185f92d118e
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-05-28 23:35:59 +0200

    Add Position model

---
+++ CarShowroom/Models/Position.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CarShowroom.Models
+{
+	public class Position
+	{
+		public int PositionId { get; set; }
+		public string Title { get; set; }
+		public decimal Salary { get; set; }
+		public bool IsFullTime { get; set; }
+		public bool IsContract { get; set; }
+
+		public virtual ICollection<Worker> Workers { get; set; }
+	}
+}
