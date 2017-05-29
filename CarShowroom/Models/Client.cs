cd8ac9b9fcd1a70e7e86d282a165d814e2f3685d
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-05-29 18:57:03 +0200

    Add Client validation

--- CarShowroom/Models/Client.cs
+++ CarShowroom/Models/Client.cs
@@ -1,15 +1,30 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CarShowroom.Models
 {
+	[DisplayName("Klient")]
 	public class Client
 	{
 		public int ClientId { get; set; }
+		[DisplayName("Nazwisko")]
+		[StringLength(50, MinimumLength = 2, ErrorMessage = "Maksymalnie 50 znaków"), Required(ErrorMessage = "Pole wymagane")]
 		public string LastName { get; set; }
+		[DisplayName("Imię")]
+		[StringLength(50, MinimumLength = 2, ErrorMessage = "Maksymalnie 50 znaków"), Required(ErrorMessage = "Pole wymagane")]
 		public string FirstName { get; set; }
+		[DisplayName("CNUM")]
+		[StringLength(11, MinimumLength = 11, ErrorMessage = "Maksymalnie 50 znaków"), Required(ErrorMessage = "Pole wymagane")]
 		public string Pesel { get; set; }
+		[DisplayName("Miasto")]
+		[StringLength(11, MinimumLength = 2, ErrorMessage = "Maksymalnie 50 znaków"), Required(ErrorMessage = "Pole wymagane")]
 		public string City { get; set; }
+		[DisplayName("Ulica")]
+		[StringLength(11, MinimumLength = 2, ErrorMessage = "Maksymalnie 50 znaków"), Required(ErrorMessage = "Pole wymagane")]
 		public string Street { get; set; }
+		[DisplayName("Numer")]
+		[Range(1, 10000, ErrorMessage = "Numer musi być od 1 do 10000"), Required(ErrorMessage = "Pole wymagane")]
 		public int StreetNumber { get; set; }
 
 		public virtual ICollection<Purchase> Purchases { get; set; }
