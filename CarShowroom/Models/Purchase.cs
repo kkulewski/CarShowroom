1b26ad216f3dd34bdf508a66d3a3fb508311c3b6
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-06-02 00:01:48 +0200

    Reorder Purchase model attributes

--- CarShowroom/Models/Purchase.cs
+++ CarShowroom/Models/Purchase.cs
@@ -8,19 +8,29 @@ namespace CarShowroom.Models
 	public class Purchase
 	{
 		public int PurchaseId { get; set; }
+
 		[DisplayName("Klient")]
+		[Required(ErrorMessage = "Pole wymagane")]
 		public int ClientId { get; set; }
+
 		[DisplayName("Pracownik")]
+		[Required(ErrorMessage = "Pole wymagane")]
 		public int WorkerId { get; set; }
+
 		[DisplayName("Samochód")]
+		[Required(ErrorMessage = "Pole wymagane")]
 		public int CarId { get; set; }
-		[DisplayFormat(DataFormatString = "{0:dd/MM/yyy}", ApplyFormatInEditMode = true)]
+
 		[DisplayName("Data")]
+		[Required(ErrorMessage = "Pole wymagane")]
+		[DisplayFormat(DataFormatString = "{0:dd/MM/yyy}", ApplyFormatInEditMode = true)]
 		[DataType(DataType.Date, ErrorMessage = "Wartość musi być datą")]
 		public DateTime TransactionDate { get; set; }
 
 		public virtual Client Client { get; set; }
+
 		public virtual Worker Worker { get; set; }
+
 		public virtual Car Car { get; set; }
 	}
 }
