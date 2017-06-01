8a1022ddd94ec4a6b4d69ca5d3dd19ef0631af27
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-06-01 23:52:06 +0200

    Reorder Position model attributes

--- CarShowroom/Models/Position.cs
+++ CarShowroom/Models/Position.cs
@@ -8,15 +8,21 @@ namespace CarShowroom.Models
 	public class Position
 	{
 		public int PositionId { get; set; }
+
 		[DisplayName("Tytuł")]
-		[StringLength(50, MinimumLength = 2, ErrorMessage = "Tytuł musi mieć od 2 do 50 znaków"), Required(ErrorMessage = "Pole wymagane")]
+		[Required(ErrorMessage = "Pole wymagane")]
+		[StringLength(50, MinimumLength = 2, ErrorMessage = "Tytuł musi mieć od 2 do 50 znaków")]
 		public string Title { get; set; }
+
 		[DisplayName("Pensja")]
+		[Required(ErrorMessage = "Pole wymagane")]
 		[DataType(DataType.Currency, ErrorMessage = "Wartość musi być kwotą")]
-		[Range(1, 5000000, ErrorMessage = "Pensja musi być od 1 do 5000000"), Required(ErrorMessage = "Pole wymagane")]
+		[Range(1, 5000000, ErrorMessage = "Pensja musi być od 1 do 5000000")]
 		public decimal Salary { get; set; }
+
 		[DisplayName("Pełny etat")]
 		public bool IsFullTime { get; set; }
+
 		[DisplayName("Kontrakt")]
 		public bool IsContract { get; set; }
 
