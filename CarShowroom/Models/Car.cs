cf21d6f99df5220653bfc7bc1246909d579b4598
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-06-01 23:51:46 +0200

    Reorder Car model attributes

--- CarShowroom/Models/Car.cs
+++ CarShowroom/Models/Car.cs
@@ -8,16 +8,23 @@ namespace CarShowroom.Models
 	public class Car
 	{
 		public int CarId { get; set; }
+
 		[DisplayName("Marka")]
-		[StringLength(50, MinimumLength = 2, ErrorMessage = "Marka musi mieć od 2 do 50 znaków"), Required(ErrorMessage = "Pole wymagane")]
+		[Required(ErrorMessage = "Pole wymagane")]
+		[StringLength(50, MinimumLength = 2, ErrorMessage = "Marka musi mieć od 2 do 50 znaków")]
 		public string Brand { get; set; }
+
 		[DisplayName("Model")]
-		[StringLength(50, MinimumLength = 2, ErrorMessage = "Model musi mieć od 2 do 50 znaków"), Required(ErrorMessage = "Pole wymagane")]
+		[Required(ErrorMessage = "Pole wymagane")]
+		[StringLength(50, MinimumLength = 2, ErrorMessage = "Model musi mieć od 2 do 50 znaków")]
 		public string Model { get; set; }
+
 		[DisplayName("Cena")]
+		[Required(ErrorMessage = "Pole wymagane")]
 		[DataType(DataType.Currency, ErrorMessage = "Wartość musi być kwotą")]
-		[Range(1000, 5000000, ErrorMessage = "Cena musi być od 1000 do 5000000"), Required(ErrorMessage = "Pole wymagane")]
+		[Range(1000, 5000000, ErrorMessage = "Cena musi być od 1000 do 5000000")]
 		public decimal Price { get; set; }
+
 		[DisplayName("Nowy")]
 		public bool IsNew { get; set; }
 
