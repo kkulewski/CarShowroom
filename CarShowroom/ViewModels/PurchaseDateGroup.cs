0cf3ac032e7f88d8eb73520c3fcafda53e3e87c0
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-06-06 21:15:37 +0200

    Change data format in Purchase summary

--- CarShowroom/ViewModels/PurchaseDateGroup.cs
+++ CarShowroom/ViewModels/PurchaseDateGroup.cs
@@ -6,6 +6,7 @@ namespace CarShowroom.ViewModels
 	public class PurchaseDateGroup
 	{
 		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{0:dd/MM/yyy}", ApplyFormatInEditMode = true)]
 		public DateTime? TransactionDate { get; set; }
 
 		public int PurchaseCount { get; set; }
